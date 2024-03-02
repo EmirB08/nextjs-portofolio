0ebf82d0b1a206b5a2bf650cdf8a8f227c7cb59a
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-03-02 14:49:53 +0100

    color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
 				<ModeToggle />
 			</div>
 				<Navbar />
-				<h2 id="about" className="text-4xl font-bold mb-1 bg-clip-text text-teal-600">
+				<h2 id="about" className="text-4xl font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
 					Emir Baftiarovic - Front-End Developer
 				</h2>
 				<p className="text-sm md:text-sm lg:text-base mt-6 text-gray-950 dark:text-gray-300 mb-32 font-light">
@@ -28,13 +28,13 @@ const Home: React.FC = () => {
 					reiciendis, quibusdam consequuntur laboriosam molestiae perspiciatis
 					quesi.
 				</p>
-				<h2 id="skills" className="text-4xl text-glossy font-bold mb-1 bg-clip-text text-teal-600">
+				<h2 id="skills" className="text-4xl text-glossy font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
 					Skills
 				</h2>
 				<SkillsAccordion />
 
 				<div className="mt-32">
-					<h2 id="projects" className="text-4xl text-glossy font-bold mt-32 bg-clip-text text-teal-600 mb-4">
+					<h2 id="projects" className="text-4xl text-glossy font-bold mt-32 bg-clip-text dark:text-teal-600 text-teal-800 mb-4">
 						Projects
 					</h2>
 					<Projects />
