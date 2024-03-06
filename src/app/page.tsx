0d99fcfc87ef3021ffafa07388a1d9f1550fc2b1
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-03-06 13:07:09 +0100

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,31 +10,31 @@ import ContactForm from "@/components/contact-form";
 const Home: React.FC = () => {
 	return (
 		<>
-			
 			<MainContainer>
-			<div className="absolute top-1 right-1">
-				<ModeToggle />
-			</div>
+				<div className="absolute top-1 right-1">
+					<ModeToggle />
+				</div>
 				<Navbar />
-				<h2 id="about" className="text-4xl font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
+				<h2
+					id="about"
+					className="text-4xl font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
 					Emir Baftiarovic - Front-End Developer
 				</h2>
-				<p className="text-sm md:text-sm lg:text-base mt-6 text-gray-950 dark:text-gray-300 mb-32 font-light">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-					voluptatum consequatur saepe nesciunt eius distinctio voluptas
-					assumenda eos fugit consectetur incidunt suscipit explicabo temporibus
-					maiores laborum, quasi harum alias sapiente nemo itaque repudiandae
-					voluptate quis. Id alias sunt velit molestias, porro soluta, corrupti
-					reiciendis, quibusdam consequuntur laboriosam molestiae perspiciatis
-					quesi.
+				<p className="text-sm md:text-sm lg:text-base mt-2 text-gray-950 dark:text-gray-300 mb-32 font-light shadow-transparent">
+					Front-end developer working towards full-stack development.
+					Focused on modern and efficient solutions to web development in both design and code.
 				</p>
-				<h2 id="skills" className="text-4xl text-glossy font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
+				<h2
+					id="skills"
+					className="text-4xl text-glossy font-bold mb-1 bg-clip-text dark:text-teal-600 text-teal-800">
 					Skills
 				</h2>
 				<SkillsAccordion />
 
 				<div className="mt-32">
-					<h2 id="projects" className="text-4xl text-glossy font-bold mt-32 bg-clip-text dark:text-teal-600 text-teal-800 mb-4">
+					<h2
+						id="projects"
+						className="text-4xl text-glossy font-bold mt-32 bg-clip-text dark:text-teal-600 text-teal-800 mb-4">
 						Projects
 					</h2>
 					<Projects />
--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -7,49 +7,77 @@ import {
 
 const SkillsAccordion = () => (
 	<Accordion type="single" collapsible className="w-full">
-		<AccordionItem value="item-1">
-			<AccordionTrigger>Languages</AccordionTrigger>
+		<AccordionItem value="item-1" className="text-sm">
+			<AccordionTrigger>Front-end</AccordionTrigger>
 			<AccordionContent>
 				<p className="text-gray-900 dark:text-gray-300 font-light text-md">
 					Experienced with{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">JavaScript</span>
-					,transitioning to{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">TypeScript</span> for
-					better type safety. Learning{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">C#</span> as I am
-					interested in back-end development. I am also spending some time with{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">Lua</span> in my free time
-					for WoW Addon development.
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						JavaScript/TypeScript
+					</span>{" "}
+					and building web applications using{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						React/Vite
+					</span>{" "}
+					and{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						Next.js
+					</span>
+					. For UX/UI I have focused on learning component libraries like{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						MUI
+					</span>
+					,{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						JoyUI
+					</span>
+					, and{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						shadcn/ui
+					</span>{" "}
+					for minimalist & responsive web design and{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						Tailwind CSS
+					</span>{" "}
+					for CSS-in-JS.
 				</p>
 			</AccordionContent>
 		</AccordionItem>
-		<AccordionItem value="item-2">
-			<AccordionTrigger>Frameworks</AccordionTrigger>
+		<AccordionItem value="item-2" className="text-sm">
+			<AccordionTrigger>Back-end</AccordionTrigger>
 			<AccordionContent>
 				<p className="text-gray-900 dark:text-gray-300 font-light text-">
-					Experienced with{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">React/Vite</span> and{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">Next.js</span> for
-					front-end development. Learning{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">ASP.NET/.NET</span> and{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">Express.js</span> for
-					back-end development.
+					I am currently focused on learning{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						Node.js/Express.js
+					</span>{" "}
+					and{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						MongoDB/PostgreSQL
+					</span>
+					for database management. I am also looking to get into{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						C#{" "}
+					</span>
+					and{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+						ASP.NET/.NET
+					</span>
+					for back-end application development.
 				</p>
 			</AccordionContent>
 		</AccordionItem>
-		<AccordionItem value="item-3">
-			<AccordionTrigger>UX/UI</AccordionTrigger>
+		<AccordionItem value="item-3" className="text-sm">
+			<AccordionTrigger>Other</AccordionTrigger>
 			<AccordionContent>
 				<p className="text-gray-900 dark:text-gray-300 font-light text-md">
-					Have a good understanding of UX/UI principles. Experienced with design
-					in <span className="dark:text-teal-600 text-teal-800 font-medium">Figma</span> and using
-					component libraries like{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">MUI</span>,{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">JoyUI</span>, and{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">shadcn/ui</span> for
-					minimalist & responsive web design. Using{" "}
-					<span className="dark:text-teal-600 text-teal-800 font-medium">Tailwind CSS</span> for
-					CSS-in-JS.
+					As a personal hobby I have also been learning{" "}
+					<span className="dark:text-teal-600 text-teal-800 font-medium">
+					Lua
+					</span>{" "}
+					to develop WoW Addons. I also have a strong interest in A.I tools like
+					Stable Diffusion for image generation and Topaz Labs for image & video
+					upscaling using A.I.
 				</p>
 			</AccordionContent>
 		</AccordionItem>
